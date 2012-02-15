eaa7da74f78f8c2d6db9b0ce36aa5fa3da556245
Folashade <foo@andrew.cmu.edu>
2012-02-15 15:32:29 -0500

    Created Creamery Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creamery::Application.config.secret_token = 'b1d0269e70a288e71ef850668514f357c3bc736acf81d66c219a1cbf1dab64358dc2e44c481cf6600b404818e922fa3117d715170039a133133cfe2cdadd1c67'
